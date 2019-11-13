48063662065e02a1c2c254c23354dfc79ac6f3fe
rajat-webonise <rajat.kumar@weboniselab.com>
2019-11-13 15:38:40 +0530

    Second Push

---
+++ src/components/App.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100vh;
+    font-family: 'Open Sans', sans-serif;
+`;
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+    width: 500px;
+    height: 40%;
+`;
+
+const Title = styled.h1`
+    color: green;
+    font-size: 2.5rem;
+    font-weight: 700;
+`;
+
+const Paragraph = styled.p`
+    color: black;
+    font-size: 1rem;
+`;
+
+const Link = styled.a`
+    color: #00a8e8;
+`;
+
+const App = () => (
+    <Container>
+        <Wrapper>
+            <Title>
+                <span role="img" aria-label="star">
+                🍀
+                </span>{' '}
+                React Green
+            </Title>           
+            <Paragraph>
+                 Check my Github Profile
+                <Link href="https://github.com/kumarRajat77" target="_blank">
+                    here.
+                </Link>
+            </Paragraph>
+        </Wrapper>
+    </Container>
+);
+
+export default App;
